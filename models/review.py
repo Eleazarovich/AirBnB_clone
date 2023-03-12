969ffe51e945c6e2faa9292db909d69a622eb639
Eleazar Nhamuave <eleazarovich176@gmail.com>
2023-03-12 12:06:24 +0200

    review class

---
+++ models/review.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+module for the review class
+"""
+from models.base_models import BaseModel
+
+
+class Review(BaseModel):
+    """Review class that inherits from the BaseModel"""
+    place_id = ""
+    user_id = ""
+    text = ""
