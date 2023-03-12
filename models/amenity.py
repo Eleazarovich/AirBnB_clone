c0f4774934ae0fc2cef9d534241fe7e0d422c3a1
Eleazar Nhamuave <eleazarovich176@gmail.com>
2023-03-12 11:57:05 +0200

    amenity class

---
+++ models/amenity.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+module for the amenity class
+"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """amenity class that inherits from the BaseModel"""
+    name = ""
