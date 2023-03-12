359b2eba306ce8092d5e2bc869f6b07ff8836c38
Eleazar Nhamuave <eleazarovich176@gmail.com>
2023-03-12 18:40:29 +0200

    minor fixes on the file storage class

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,6 +2,7 @@
 """Module for FileStorage"""
 import json
 import datetime
+import os
 
 
 class FileStorage:
@@ -89,12 +90,10 @@ class FileStorage:
         (__file_path) exists ; otherwise, do nothing. If the file doesn’t
         exist, no exception should be raised)
         """
-        d = {}
-        try:
-            with open(self.__file_path, "r", encoding="utf-8") as f:
-                obj_dict = json.load(f)
-                obj_dict = {k: self.classes()[v["__class__"]](**v)
-                            for k, v in obj_dict.items()}
-                FileStorage.__objects = obj_dict
-        except FileNotFoundError:
-            pass
+        if not os.path.isfile(FileStorage.__file_path):
+            return
+        with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
+            d = json.load(f)
+            d = {k: self.classes()[v["__class__"]](**v)
+                 for k, v in d.items()}
+            FileStorage.__objects = d
