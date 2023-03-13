63cc0da66607d55c725f5caecbe9cd6554118e26
Eleazar Nhamuave <eleazarovich176@gmail.com>
2023-03-13 04:10:44 +0200

    update base model class

--- models/base_model.py
+++ models/base_model.py
@@ -10,15 +10,15 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """initialization method"""
         if kwargs is not None and kwargs != {}:
-            for k in kwargs:
-                if k == "created_at":
+            for key in kwargs:
+                if key == "created_at":
                     self.__dict__["created_at"] = datetime.strptime(
                         kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
-                elif k == "updated_at":
+                elif key == "updated_at":
                     self.__dict__["updated_at"] = datetime.strptime(
                         kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
                 else:
-                    self.__dict__[k] = kwargs[k]
+                    self.__dict__[key] = kwargs[key]
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
